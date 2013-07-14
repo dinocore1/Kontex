1b56be4f66f2796258830aff98899375901ae8e9
Paul Soucy <paul@paul-VirtualBox.(none)>
2013-07-14 17:28:44 -0400

    more renaming

--- src/main/java/org/devsmart/kontex/bencode/BDecoder.java
+++ src/main/java/org/devsmart/kontex/bencode/BDecoder.java
@@ -18,7 +18,7 @@
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-package org.devsmart.kontext.bencode;
+package org.devsmart.kontex.bencode;
 
 import java.io.EOFException;
 import java.io.IOException;
